59be654d7fef3cd9f0bfed8f857dbb527265e972
Rom555 <romanzubrilin@gmail.com>
2022-02-03 15:55:29 +0300

    code fix and clean

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
 "use strict";
 
 const isNumber = function (num) {
-  return !isNaN(parseInt(num)) || isFinite(num) || num !== "";
+  return !isNaN(parseInt(num)) && isFinite(num) && num !== "";
 };
 
 const gameStart = function () {
   let number = Math.ceil(Math.random() * 100 + 1);
 
   const game = function (str) {
-    let userNumber = prompt(str).trim();
+    let userNumber = prompt(str);
 
     if (userNumber == null) {
       alert("Игра окончена");
-    } else if (isNumber(userNumber)) {
-      userNumber = +userNumber;
+      return;
+    }
 
-      if (userNumber === number) {
-        alert("Поздравляю, Вы угадали!!!");
-      } else if (userNumber > number) {
-        game("Загаданное число меньше");
-      } else if (userNumber < number) {
-        game("Загаданное число больше");
-      }
-    } else {
+    userNumber = userNumber.trim();
+
+    if (!isNumber(userNumber)) {
       game("Введи число!");
+      return;
+    }
+
+    userNumber = +userNumber;
+
+    if (userNumber === number) {
+      alert("Поздравляю, Вы угадали!!!");
+    } else if (userNumber > number) {
+      game("Загаданное число меньше");
+    } else if (userNumber < number) {
+      game("Загаданное число больше");
     }
   };
 
